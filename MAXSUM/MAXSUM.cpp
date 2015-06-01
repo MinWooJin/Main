c8482f326c78e31c046093f4ae8f7e8b58c16630
MinWooJin <alsdn653@naver.com>
2015-06-01 11:55:58 +0900

    algospot/MAXSUM problem with C++
    https://algospot.com/judge/problem/read/MAXSUM

--- MAXSUM/MAXSUM.cpp
+++ MAXSUM/MAXSUM.cpp
@@ -1,54 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 int main() {
+	int sum, n, max, input;
+	int testcase;
+	cin>>testcase;
+	
+	for(int i=0; i<testcase; i++) {
+		sum=0;
+		max=0;
+		cin>>n;
 
-	int testcase, i, j, linenum[100], contisum, maxsum[100];
-	int ary[100][1000];
-	scanf("%d",&testcase);
-
-	for(i=0; i<testcase; i++) {
-		scanf("%d",&linenum[i]);
-		for(j=0; j<linenum[i]; j++)
-			scanf("%d",&ary[i][j]);
-	}
-
-	for(i=0; i<testcase; i++) {
-		contisum=0;
-		if(ary[i][0]>0)
-			maxsum[i]=0;
-		else
-			maxsum[i]=-1;
-		for(j=0; j<linenum[i]; j++) {
-			if(ary[i][j] >= 0) {
-				if(ary[i][j] == ary[i][j+1]-1 && ary[i][j+1] == ary[i][j+2]-1) {
-					contisum=contisum+ary[i][j];
-					if(maxsum[i] < contisum)
-						maxsum[i] = contisum;
-				}
-				else if(ary[i][j] == ary[i][j+1]-1 ) {
-					contisum=contisum+ary[i][j]+ary[i][j+1];
-					if(maxsum[i] < contisum)
-						maxsum[i] = contisum;
-				}
-			}
-			else {
-				if(ary[i][j]-1 == ary[i][j+1] && ary[i][j+1]-1 != ary[i][j+2]) {
-					contisum=contisum+ary[i][j]+ary[i][j+1];
-					if(maxsum[i] < contisum)
-						maxsum[i] = contisum;
-				}
-				else if(ary[i][j]-1 == ary[i][j+1]) {
-					contisum=contisum+ary[i][j];
-					if(maxsum[i] < contisum)
-						maxsum[i] = contisum;
-				}
-			}
+		for(int j=0; j<n; j++) {
+			cin>>input;
+			
+			if(sum>0)
+				sum+=input;
+			else
+				sum=input;
+			if(max < sum)
+				max = sum;
 		}
-	}
-	for(i=0; i<testcase; i++)
-		printf("%d\n",maxsum[i]);
 
-	system("pause");
+		cout << max << endl;
+
+	}
 	return 0;
 }
